3dea499eb7e487ca9cfd6028c5a93db67cb11309
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2024-05-19 21:38:23 +0530

    Distance travelled

--- Distance travelled.py
+++ Distance travelled.py
@@ -1,3 +1,3 @@
-s,t=int(input("Enter speed in km/hr:")),int(input("Enter time in minutes:"))
+s,t=int(input("Enter speed in km/hr: ")),int(input("Enter time in minutes: "))
 d=s*t
 print("Distance travelled=",d)
